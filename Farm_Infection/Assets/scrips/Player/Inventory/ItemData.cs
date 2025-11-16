af9107509cbaa58bff659818b8e7d1beb77077e8
ExtraSeven-code <apexexe124@gmail.com>
2025-11-16 13:54:57 -0500

    planta
    planta papa

--- Farm_Infection/Assets/scrips/Player/Inventory/ItemData.cs
+++ Farm_Infection/Assets/scrips/Player/Inventory/ItemData.cs
@@ -1,19 +1,25 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/Item")]
 public class ItemData : ScriptableObject
 {
-    [Header("Datos b�sicos")]
+    [Header("Datos básicos")]
     public string id;
     public string displayName;
     public Sprite icon;
 
-    [Header("Configuraci�n")]
+    [Header("Configuración")]
     public int maxStack = 64;
     public bool isConsumable;
 
     [Header("Prefab en el mundo 3D")]
     public GameObject worldPrefab;
+
+    [Header("Semilla / Planta (opcional)")]
+    public bool isSeed;                 // ✅ marcar si este item es una semilla
+    public GameObject plantPrefab;      // modelo de la planta en la maceta
+    public ItemData harvestItem;        // fruto que dará al cosechar
+    public int harvestAmount = 1;
 }
